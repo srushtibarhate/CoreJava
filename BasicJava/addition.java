688b3b5f460df70c8a13b06626be788d1c4d2c15
srushtibarhate <shrushtibarhate@gmail.com>
2023-03-01 20:04:33 +0530

    basic2

---
+++ BasicJava/addition.java
@@ -0,0 +1,13 @@
+package Basicjava;
+import java.util.Scanner;
+public class addition {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a,b,c;
+System.out.println("enter 2 number");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println("addition is"+c);
+   } 
+}
